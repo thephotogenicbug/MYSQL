805eef00d883a4dd2866e40f7b418801cf06213b
thephotogenicbug <navinkumarachari@gmail.com>
2024-09-25 12:46:20 +0530

    mysql create, drop , use, show databases commands

---
+++ practical_01/create_drop_use_show_database.sql
@@ -0,0 +1,18 @@
+
+-- create a database if not exists
+CREATE DATABASE school;
+
+-- create a database if not exists
+CREATE DATABASE IF NOT EXISTS school;
+
+-- drop a database
+DROP DATABASE school;
+
+-- drop a database if exists;
+DROP DATABASE IF EXISTS school;
+
+-- use database
+USE school;
+
+-- show database
+SHOW DATABASES;
