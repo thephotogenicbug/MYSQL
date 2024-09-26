985ad98c3f98051c83890120e5710df1453c1220
thephotogenicbug <navinkumarachari@gmail.com>
2024-09-26 12:51:52 +0530

    update primary key and foreign key commands

--- practical_01/primary_key_foreign_key_constraints.sql
+++ practical_01/primary_key_foreign_key_constraints.sql
@@ -1,11 +1,15 @@
 
 USE college;
 
+-- base table
 CREATE TABLE student(
  id      INT PRIMARY KEY,
  name    VARCHAR(30)
 );
 
+-- child table
 CREATE TABLE course(
- 
+ cid     INT PRIMARY KEY,
+ id      INT,
+ FOREIGN KEY(id) REFERENCES student(id)
 );
